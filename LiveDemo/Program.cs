4b4a23d885a23032496710c469d07bbc62f6a7b6
niklas-hjelm <niklas.hjelm@iths.se>
2023-02-06 13:33:03 +0100

    hello

--- LiveDemo/Program.cs
+++ LiveDemo/Program.cs
@@ -13,5 +13,20 @@ var names = new List<string>(){"Niklas", "Thilde", "Vidar"};
 app.MapGet("/names", () => names);
 app.MapPost("/names/{newName}", (string newName) => names.Add(newName));
 
+var people = new Dictionary<Guid, Person>();
+
+app.MapPost("/savePerson", (Person person) =>
+{
+    people.Add(Guid.NewGuid(), person);
+});
+
+app.MapGet("/getPeople", () => people);
+
 
 app.Run();
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
